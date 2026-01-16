f65471f80297eb3bfc38cefc431cb97a11c7ab7f
WGTH <somphong.ww999@gmail.com>
2026-01-16 14:36:03 +0700

    commit

---
+++ backend/src/index.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import { ENV } from "./config/env"
+import { clerkMiddleware } from '@clerk/express'
+
+const app = express()
+
+app.use(cors({ origin: ENV.FRONTEND_URL }));
+app.use(clerkMiddleware());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+ 
+    app.get("/", (req, res) => {
+    res.json({
+        message: "Welcome to Product API - Power by PostgreSQL,",
+        endpoints: {
+            users: "/api/users",
+            products: "/api/products",
+            comments: "/api/comments",
+        },
+    });
+});
+
+app.listen(ENV.PORT, () => console.log("Server is up on PORT:", ENV.PORT))
